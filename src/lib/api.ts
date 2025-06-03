1386cb207fd99f969875b7cf2e544ff35cbe9ebe
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-06-03 09:09:35 +0700

    feat: move Content-Type header to jsonRequest function for better request handling

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,10 +2,26 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8000/v1/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
   withCredentials: true,
 });
+
+export const jsonRequest = (url: string, method: string, data?: any) => {
+  return api({
+    url,
+    method,
+    data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+export const formRequest = (url: string, method: string, data?: FormData) => {
+  return api({
+    url,
+    method,
+    data,
+  });
+};
 
 export default api;
