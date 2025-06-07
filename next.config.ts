b0547638f360fc69f17072233f16a51b6bde168e
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-06-07 10:09:07 +0700

    feat: add additional image domain to next.config for improved image loading

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["akcdn.detik.net.id"],
+    domains: ["akcdn.detik.net.id", "cdn.detik.net.id"],
   },
 };
 
