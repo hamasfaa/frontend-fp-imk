130fa44768e024d227e14e2e6af4b96351b7e5dc
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-06-24 17:56:12 +0700

    refactor: optimize user fetching logic in useMe hook with useCallback and add refetchUser functionality

--- src/app/profile/hooks/useMe.tsx
+++ src/app/profile/hooks/useMe.tsx
@@ -1,43 +1,45 @@
 "use client";
 
 import { jsonRequest } from "@/lib/api";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export function useMe() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await jsonRequest("/me", "GET");
+  const fetchUser = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await jsonRequest("/me", "GET");
 
-        if (!response || response.status !== 200) {
-          throw new Error("Gagal mengambil data pengguna");
-        }
-        const data = response.data;
-        const formattedUser = {
-          username: data.data.username,
-          firstName: data.data.first_name,
-          lastName: data.data.last_name,
-          email: data.data.email,
-          address: data.data.address,
-          phone: data.data.phone,
-          points: data.data.points,
-          rank: data.data.rank,
-        };
-        setUser(formattedUser);
-      } catch (error) {
-        console.error("Error fetching user:", error);
-        setError(error instanceof Error ? error.message : "Terjadi kesalahan");
-      } finally {
-        setLoading(false);
+      if (!response || response.status !== 200) {
+        throw new Error("Gagal mengambil data pengguna");
       }
-    };
-
-    fetchUser();
+      const data = response.data;
+      const formattedUser = {
+        username: data.data.username,
+        firstName: data.data.first_name,
+        lastName: data.data.last_name,
+        email: data.data.email,
+        address: data.data.address,
+        phone: data.data.phone,
+        points: data.data.points,
+        rank: data.data.rank,
+      };
+      setUser(formattedUser);
+      setError(null);
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      setError(error instanceof Error ? error.message : "Terjadi kesalahan");
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
-  return { user, loading, error };
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
+  return { user, loading, error, refetchUser: fetchUser };
 }
